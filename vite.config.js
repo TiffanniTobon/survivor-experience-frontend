97b116a310b1d7d7068cdf9917867877d5ef33fd
TiffaTobon <tiffanni.tobona@comunidad.iush.edu.co>
2026-03-20 18:17:17 -0500

    init: scaffold react vite project

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000', // puerto donde corre tu back
+        changeOrigin: true,
+      },
+    },
+  },
+})
